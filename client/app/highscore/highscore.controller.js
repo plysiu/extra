c33d3e4bc4bfca2e663b8358173cfaa9debfa318
Przemysław Płyś <przemyslaw.plys@gmail.com>
2016-01-14 21:13:58 +0100

    Added feedback text

--- client/app/highscore/highscore.controller.js
+++ client/app/highscore/highscore.controller.js
@@ -1,11 +1,59 @@
 'use strict';
 
 angular.module('devMashApp')
-  .controller('HighscoreCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+  .controller('HighscoreCtrl', ['$scope', '$http', '$routeParams', '$location', function ($scope, $http, $routeParams, $location) {
 
     $scope.globalHighscore = [];
-
     $scope.localHighscore = [];
+
+    $scope.show = false;
+    $scope.session = true;
+
+
+    $scope.clickShow = function () {
+      $scope.show = true;
+    }
+
+    $scope.homepage = function () {
+      $location.path('/');
+    };
+
+
+    $scope.data = '';
+
+
+    $scope.sendData = function () {
+
+      if (typeof $routeParams.sessionId !== 'undefined') {
+        $http.post('/api/sessions', {session: $routeParams.sessionId, text: $scope.data})
+          .success(function (data) {
+console.log(data);
+            $scope.show = true;
+          }).error(function (err) {
+          $scope.show = true;
+        });
+      }
+    }
+
+    if (typeof $routeParams.sessionId !== 'undefined') {
+
+      $http.get('api/sessions/' + $routeParams.sessionId)
+        .success(function (data) {
+          console.log(data);
+          if (data) {
+            $scope.show = true;
+
+          }
+
+        }).error(function () {
+        $scope.show = true;
+        $scope.session = false;
+      });
+    }
+    else {
+      $scope.show = true;
+      $scope.session = false;
+    }
 
     if (typeof $routeParams.timetableId !== 'undefined') {
 
@@ -16,7 +64,7 @@ angular.module('devMashApp')
           var max = local[0].points;
           var min = local[local.length - 1].points;
           for (var i = 0; i < local.length; i++) {
-            local[i].percent =parseInt( 100 * (local[i].points - min) / (max - min));
+            local[i].percent = parseInt(100 * (local[i].points - min) / (max - min));
           }
           $scope.globalHighscore = local;
         });
@@ -30,7 +78,8 @@ angular.module('devMashApp')
           var max = local[0].points;
           var min = local[local.length - 1].points;
           for (var i = 0; i < local.length; i++) {
-            local[i].percent =parseInt( 100 * (local[i].points - min) / (max - min));          }
+            local[i].percent = parseInt(100 * (local[i].points - min) / (max - min));
+          }
           $scope.highscore = local;
         });
     }
@@ -43,13 +92,15 @@ angular.module('devMashApp')
           var max = local[0].points;
           var min = local[local.length - 1].points;
           for (var i = 0; i < local.length; i++) {
-            local[i].percent =parseInt( 100 * (local[i].points - min) / (max - min));          }
+            local[i].percent = parseInt(100 * (local[i].points - min) / (max - min));
+          }
 
           $scope.localHighscore = local;
         });
 
     }
-  }])
+  }
+  ])
 ;
 
 
